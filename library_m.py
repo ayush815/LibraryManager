fb0d486965b66fe69dde8fd6f8ec66bd8da679ee
ayush815 <103514684+ayush815@users.noreply.github.com>
2022-08-28 16:15:59 +0530

    Add files via upload

---
+++ library_m.py
@@ -0,0 +1,54 @@
+class Library:
+    def __init__(self, listOfBooks):
+        self.books = listOfBooks
+    def displayAvailableBooks(self):
+        print("Books present in this library are: ")
+        for book in self.books:
+            print("\t*" +book)
+    def borrowBook(self, bookName):
+        if bookName in self.books:
+            print(f"You have been issued {bookName}. Please keep it safe and return within 30 days")
+            self.books.remove(bookName)
+            return True
+        else:
+            print("Sorry, This book is either not available or already issued to someone else. Please wait until the book is returned")
+            return False
+    def returnBook(self, bookName):
+        self.books.append(bookName)
+        print("Thanks for returning this book! Hope you enjoyed reading it. Have a great day ahead!")
+
+class student:
+    def requestBook(self):
+        self.book = input("Enter the name of the book you want to borrow: ")
+        return self.book
+
+    def returnBook(self):
+        self.book = input("Enter the name of the book you want to return: ")
+        return self.book
+
+if __name__ == "__main__":
+    centralLibrary = Library(["Algorothem", "Python", "Django", "Flask", "HTML"])
+    student = student()
+    # centralLibrary.displayAvailableBooks()
+    while(True):
+        welcomeMsg = '''\n ===== Welcome to central Library =====
+        Please choose an option: 
+        1. List all the books
+        2. Request a book
+        3. Add or return a book
+        4. Exit the library
+        '''
+        print(welcomeMsg)
+        a = int(input("Enter a choise: "))
+        if a == 1:
+            centralLibrary.displayAvailableBooks()
+        elif a == 2:
+            centralLibrary.borrowBook(student.requestBook())
+        elif a == 3:
+            centralLibrary.returnBook(student.returnBook())
+        elif a == 4:
+            print("Thanks for using Central Library. Have a great day!")
+            exit()
+        else:
+            print("Invalid choice!")
+    
